9a349e563da6544dea95babf3daaa8e6557faba9
daniel-inwood <info@daniel-inwood.com>
2022-10-23 17:48:18 +0100

    Adding 1, 2, partial 3

---
+++ 2.ps1
@@ -0,0 +1,48 @@
+﻿
+
+
+
+
+$array = @(1, 2)
+
+
+while($array[-1] -lt 4000000){
+
+$array += ($array[-1] + $array[-2])
+
+
+
+}
+
+
+$total = 0
+
+foreach($entry in $array){
+
+if($entry % 2 -eq 0){ $total = $total + $entry}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 3.ps1
@@ -0,0 +1,52 @@
+﻿$limit = 1000
+$candidate = 2
+$primes = @(1, 2)
+while($candidate -ne $limit){
+    if($candidate % 2 -ne 0){
+        $prime = $true
+        $testnumbers = @(2..($candidate - 1))
+        foreach($testnumber in $testnumbers){
+            if($candidate % $testnumber -eq 0){$prime = $false}
+            }
+        if($prime -eq $true){
+        $primes += $candidate
+      }
+}
+$candidate++
+}
+$primes
+
+
+
+
+
+
+
+$limit = 100000
+$candidate = 4
+$primes = @(2, 3)
+while($candidate -ne $limit){
+    if($candidate % 2 -ne 0){
+        $prime = $true
+        foreach($testnumber in $primes){
+            if($candidate % $testnumber -eq 0){$prime = $false}
+            }
+        if($prime -eq $true){
+        $primes += $candidate
+      }
+}
+$candidate++
+}
+$primes
+
+
+
+$input = 13195
+
+
+
+
+
+
+## 5, 7, 13 and 29.
+
