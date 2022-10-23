640b12f21a1a068a468889be7c7fa60b3d754e97
daniel-inwood <info@daniel-inwood.com>
2022-10-23 17:54:25 +0100

    Let's start a naming schema

---
+++ 1 - Multiples of 3 or 5.ps1
@@ -0,0 +1,29 @@
+﻿<#
+https://projecteuler.net/problem=1
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+#>
+
+
+$Num = 0
+
+
+$output = 0
+
+while($num -lt 1000){
+
+$updateflag = $false
+if($num % 3 -eq 0){ $updateflag = $true }
+if($num % 5 -eq 0){ $updateflag = $true }
+
+if($updateflag -eq $true){ 
+$num
+$output = $output + $num}
+
+$num++
+
+}
+
+$output
