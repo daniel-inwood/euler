b6685dc79c9eb5b611cacc06bb9e2e2392206313
daniel-inwood <114866992+daniel-inwood@users.noreply.github.com>
2024-09-01 16:30:48 +0100

    Finished 18 (finally)

--- 18 - Maximum Path Sum 1.ps1
+++ 18 - Maximum Path Sum 1.ps1
@@ -30,21 +30,50 @@ Find the maximum total from top to bottom of the triangle below:
 
 NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)#>
 
+$Level0  = (75)
+$Level1  = (95, 64)
+$Level2  = (17, 47, 82)
+$Level3  = (18, 35, 87, 10)
+$Level4  = (20,  4, 82, 47, 65)
+$Level5  = (19,  1, 23, 75,  3, 34)
+$Level6  = (88,  2, 77, 73,  7, 63, 67)
+$Level7  = (99, 65,  4, 28,  6, 16, 70, 92)
+$Level8  = (41, 41, 26, 56, 83, 40, 80, 70, 33)
+$Level9  = (41, 48, 72, 33, 47, 32, 37, 16, 94, 29)
+$Level10 = (53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14)
+$Level11 = (70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57)
+$Level12 = (91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48)
+$Level13 = (63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31)
+$Level14 = ( 4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23)
 
+$Route = 0
+$BestRouteTotal = 0
 
+While ($Route -le 16384){    # 2 to the power of the number of levels of this pyramid
 
+    $RouteBinary = [convert]::ToString($Route,2)
+    $RouteBinary = $RouteBinary.PadLeft(15, "0")
+    
+    $CursorRow = 0
+    $CursorColumn = 0
+    $RouteTotal = 0
+    
+    While ($CursorRow -lt 15){
+        $Row = Get-Variable -Name ("Level" + $CursorRow) -ValueOnly
+        If ($RouteBinary[$CursorRow] -eq "1"){ $CursorColumn++ }
+        $RouteTotal += $Row[$CursorColumn]    
+        $CursorRow++
+    }
+    If ($RouteTotal -gt $BestRouteTotal){
+        $BestRouteTotal = $RouteTotal
+        $BestRouteBinary = $RouteBinary
+        }
+    $Route++
+}
 
-
-
-
-
-
-
-
-
-
-
-
+$BestRouteBinary = $BestRouteBinary -replace "0", "Left, " -replace "1", "Right, " 
+$String = "Best route: " + $BestRouteBinary + "for a toal of " + $BestRouteTotal
+$String
 
 
 
---
+++ 23 - Non-Abundant Sums.ps1
@@ -0,0 +1,83 @@
+﻿<#
+https://projecteuler.net/problem=23
+
+A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+A number (n) is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+
+#>
+
+
+Function Perfection-Checker {
+    $InputNumber = $args[0]
+    $TestNumber = $InputNumber - 1 #essential
+    $SOPD = 0
+
+    While($TestNumber -gt 0){
+        If($InputNumber % $TestNumber -eq 0){
+           # $TestNumber #########
+            $SOPD += $TestNumber
+        }
+        $TestNumber--
+    }
+
+    If($SOPD -gt $InputNumber){Return "Abundant"}
+    If($SOPD -eq $InputNumber){Return "Perfect"}
+    If($SOPD -lt $InputNumber){Return "Deficient"}
+
+}
+
+
+$AbundantNumbers = @()
+$AbundantTest = 2
+
+While($AbundantTest -lt 28123){
+    If((Perfection-Checker $AbundantTest) -eq "Abundant"){
+        $AbundantNumbers += ($AbundantTest)
+        }
+    $AbundantTest++
+    }
+
+
+
+## $AbundantNumbers | Out-File -LiteralPath "C:\Users\wigan\Desktop\Git Repo\Euler\euler\23 AbunduntNumbersList.txt"
+
+
+
+
+$AbundantSumNumbers = @()
+
+
+##### DO A NASTY CROSS JOIN AND WHATEVER IS LEFT IS THE RESULT (sum the digits)
+
+ForEach($Num1 in $AbundantNumbers){
+    $Num1
+    ForEach($Num2 in $AbundantNumbers){
+        $Sum = $Num1 + $Num2
+        If($Sum -lt 28123){
+            If($Sum -notin $AbundantSumNumbers){
+                $AbundantSumNumbers += $SizeCheck
+                }
+            }
+        }
+    }
+
+$AbundantSumNumbers.count
+
+
+
+
+
+
+
+
+
+
+
+
+
+
